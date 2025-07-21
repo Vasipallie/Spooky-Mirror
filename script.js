e104c9dae0976178b895e3112f45c9d1bd1638ea
eshangonemad <vasipallieshan@gmail.com>
2025-07-21 11:05:02 +0800

    Spooky Mirror

---
+++ script.js
@@ -0,0 +1,7 @@
+function rungame(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("webpage").style.display = "block";
+}
+
+document.getElementById("home").style.display = "block";
+document.getElementById("webpage").style.display = "none";
